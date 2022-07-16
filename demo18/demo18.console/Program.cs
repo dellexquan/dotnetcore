ae67734c15ce061960e316ce091bb4d5c44f821b
dellex <379323141@qq.com>
2022-07-16 14:39:15 +0800

    add demo18

---
+++ demo18/demo18.console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var services = new ServiceCollection();
+services.AddScoped<Test1>();
+services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddConsole();
+    loggingBuilder.SetMinimumLevel(LogLevel.Debug);
+});
+
+using (var sp = services.BuildServiceProvider())
+{
+    var test1 = sp.GetRequiredService<Test1>();
+    test1.Test();
+}
+
+// System.Console.WriteLine("DI complete!");
---
+++ demo18/demo18.console/Test1.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Logging;
+
+class Test1
+{
+    private readonly ILogger<Test1> logger;
+    public Test1(ILogger<Test1> logger)
+    {
+        this.logger = logger;
+    }
+
+    public void Test()
+    {
+        logger.LogInformation("This is an info message.");
+        logger.LogDebug("This is a debug message.");
+        logger.LogWarning("This is a warning message.");
+        logger.LogError("This is an error message.");
+
+        try
+        {
+            var content = File.ReadAllText("1.txt");
+            logger.LogInformation(content);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Fail to read from the file.");
+        }
+    }
+
+}
