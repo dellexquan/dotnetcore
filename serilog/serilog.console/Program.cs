e7a3a60eb74ada101cc0453809f1b541d10b5cd4
dellex <379323141@qq.com>
2022-07-16 18:24:04 +0800

    update exceptionless

---
+++ serilog/serilog.console/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog.Formatting.Json;
+using Exceptionless;
+
+var services = new ServiceCollection();
+services.AddScoped<Test1>();
+services.AddLogging(loggingBuilder =>
+{
+    ExceptionlessClient.Default.Startup("feMf2z4Sb6l88bbuceqJTduiBifNQcqvfmsMZNlO");
+    Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console(new JsonFormatter())
+                        .WriteTo.Exceptionless()
+                        .CreateLogger();
+    loggingBuilder.AddSerilog(dispose: true);
+});
+
+
+using (var sp = services.BuildServiceProvider())
+{
+    var test1 = sp.GetRequiredService<Test1>();
+    test1.Test();
+}
---
+++ serilog/serilog.console/Test1.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.Logging;
+
+class Test1
+{
+    private readonly ILogger<Test1> logger;
+    public Test1(ILogger<Test1> logger)
+    {
+        this.logger = logger;
+    }
+
+    public void Test()
+    {
+        logger.LogInformation("This is an info message.");
+        logger.LogDebug("This is a debug message.");
+        logger.LogWarning("This is a warning message.");
+        logger.LogError("This is an error message.");
+        // var person = new { Id = 3, Name = "Zack" };
+        logger.LogWarning("New user: {@person}", new { Id = 3, Name = "Zack" });
+        // logger.LogWarning($"New user: {person}");
+
+        try
+        {
+            var content = File.ReadAllText("1.txt");
+            logger.LogInformation(content);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Fail to read from the file.");
+        }
+    }
+
+}
