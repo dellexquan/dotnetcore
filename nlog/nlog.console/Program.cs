b193740b749aad079c20107e871b6e563ea96e36
dellex <379323141@qq.com>
2022-07-16 15:07:43 +0800

    add nlog

---
+++ nlog/nlog.console/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NLog.Extensions.Logging;
+
+var services = new ServiceCollection();
+services.AddScoped<Test1>();
+services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddNLog();
+});
+
+
+using (var sp = services.BuildServiceProvider())
+{
+    var test1 = sp.GetRequiredService<Test1>();
+    test1.Test();
+}
