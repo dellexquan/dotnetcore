2e046fd8d2c4be8bfb1a9b33e1a06ba8c759ede5
dellex <379323141@qq.com>
2022-07-23 20:28:51 +0800

    enable swagger and cors

--- miniapi/jwt/jwt.api/Program.cs
+++ miniapi/jwt/jwt.api/Program.cs
@@ -1,5 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+//register swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//register cors policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+//enable swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+//enable cors
+app.UseCors("CorePolicy");
 
 app.MapGet("/", () => "Hello World!");
 
