b6a5cf599be4e981ce93d9fdd55c142b7af533e4
dellex <379323141@qq.com>
2022-07-24 15:09:41 +0800

    fix bug

--- miniapi/jwt/jwt.api/Program.cs
+++ miniapi/jwt/jwt.api/Program.cs
@@ -1,16 +1,45 @@
 using jwt.api.models;
 using jwt.common;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 //register swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new()
+    {
+        Description = "Please input token, the format is Bearer xxxxxx (P.S. don't miss the blank in the middle.)",
+        Name = "Authorization",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        BearerFormat = "JWT",
+        Scheme = JwtBearerDefaults.AuthenticationScheme
+    });
+    var reference = new OpenApiReference()
+    {
+        Type = ReferenceType.SecurityScheme,
+        Id = JwtBearerDefaults.AuthenticationScheme
+    };
+
+    var scheme = new OpenApiSecurityScheme()
+    {
+        Reference = reference
+    };
+    var requirement = new OpenApiSecurityRequirement();
+    requirement.Add(scheme, new string[] { });
+    options.AddSecurityRequirement(requirement);
+});
 
 //register jwt
 var tokenOptions = new JWTTokenOptions();
 builder.Configuration.Bind("JWTTokenOptions", tokenOptions);
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+builder.Services.AddAuthorization(options => { })
+.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
 {
     options.TokenValidationParameters = new()
@@ -19,6 +48,17 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         ValidateAudience = true,
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
+        ValidIssuer = tokenOptions.Issuer,
+        ValidAudience = tokenOptions.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenOptions.SecurityKey ?? "")),
+        AudienceValidator = (x, y, z) =>
+        {
+            return true;
+        },
+        LifetimeValidator = (notBefore, expires, securityToken, ValidationParamters) =>
+        {
+            return true;
+        }
     };
 });
 
@@ -44,7 +84,7 @@ app.UseSwaggerUI();
 app.UseCors("CorePolicy");
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("api/get-user", () =>
+app.MapGet("api/get-user", [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)] () =>
 {
     return new User
     {
