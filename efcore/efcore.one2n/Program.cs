fb8b94de69ac3153fe2e0ff688d69e0fb68b972a
dellex <379323141@qq.com>
2022-07-25 08:40:56 +0800

    insert one new article

--- efcore/efcore.one2n/Program.cs
+++ efcore/efcore.one2n/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using (var db = new EntityDbContext())
+{
+    var a1 = new Article
+    {
+        Title = "Title1",
+        Content = "Content1",
+        Comments = new List<Comment>
+        {
+            new Comment
+            {
+                Message = "Comment1.1"
+            },
+            new Comment
+            {
+                Message = "Comment1.2"
+            }
+        }
+    };
+
+    db.Articles.Add(a1);
+    db.SaveChanges();
+    System.Console.WriteLine("Complete!");
+}
