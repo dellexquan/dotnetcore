aeec2b8f86e280745b397ec39ea5f2f488ec1329
dellex <379323141@qq.com>
2022-07-30 10:03:43 +0800

    rename efcore.one2n as efcore.relationship

--- efcore/efcore.relationship/OrgUnitConfig.cs
+++ efcore/efcore.relationship/OrgUnitConfig.cs
@@ -6,6 +6,6 @@ public class OrgUnitConifg : IEntityTypeConfiguration<OrgUnit>
     public void Configure(EntityTypeBuilder<OrgUnit> builder)
     {
         builder.Property(o => o.Name).IsRequired().IsUnicode();
-        builder.HasOne(o => o.Parent).WithMany(p => p.Children);
+        builder.HasOne(o => o.Parent).WithMany(p => p.Children); 
     }
 }
