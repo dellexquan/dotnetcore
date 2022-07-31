c700008e3eec2be46ef11693c4ba741e797053a1
dellex <379323141@qq.com>
2022-07-31 14:58:01 +0800

    add efcore.expression

---
+++ efcore/efcore.expression/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq.Expressions;
+using ExpressionTreeToString;
+
+// Expression<Func<string, bool>> expr = s => s.Count() > 5;
+// Console.WriteLine(expr.ToString("Object notation", "C#"));
+
+Expression<Func<int, int, int>> expr2 = (a, b) => a + b;
+System.Console.WriteLine(expr2.ToString("Object notation", "C#"));
