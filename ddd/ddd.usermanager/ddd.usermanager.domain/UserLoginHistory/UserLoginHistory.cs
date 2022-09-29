33d4b88b1ca1b07641e7f59dde76c01133450915
dellex <379323141@qq.com>
2022-09-29 08:44:59 +0800

    add UserLoginHistory

---
+++ ddd/ddd.usermanager/ddd.usermanager.domain/UserLoginHistory/UserLoginHistory.cs
@@ -0,0 +1,19 @@
+namespace ddd.usermanager.domain;
+
+public record UserLoginHistory : IAggregateRoot
+{
+    public long Id { get; init; }
+    public Guid UserId { get; init; }
+    public PhoneNumber PhoneNumber { get; init; } = null!;
+    public DateTime CreateTime { get; init; }
+    public string Message { get; init; } = null!;
+
+    private UserLoginHistory() { }
+    public UserLoginHistory(Guid userId, PhoneNumber phoneNumber, string message)
+    {
+        this.UserId = userId;
+        this.PhoneNumber = phoneNumber;
+        this.CreateTime = DateTime.Now;
+        this.Message = message;
+    }
+}
